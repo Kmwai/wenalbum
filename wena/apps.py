533d59305ffb83478673a19e70efd3a26d686b76
Mwai <mwai@Misfit.local>
2018-07-31 14:09:26 +0100

    project files

---
+++ wena/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WenaConfig(AppConfig):
+    name = 'wena'
---
+++ wena/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = 'name',
+        verbose_name = 'location'
+        verbose_name_plural = 'locations'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('', args=[self.slug])
+
+
+class Photo(models.Model):
+    category = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='pictures')
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='pictures/%Y/%m/%d/', blank=True)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ['name']
+        index_together = (('id', 'slug'),)  # For querying photos by both id and slug (also to improve performance)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('', args=[self.id, self.slug])
