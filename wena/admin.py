67def03035a155aeb81c321522cc095a5a7831de
Mwai <mwai@Misfit.local>
2018-09-20 14:22:33 +0100

    updated admin file

--- wena/admin.py
+++ wena/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Location, Photo
 
-# Register your models here.
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'description', 'created', 'updated']
+    list_filter = ['created', 'updated']
+    list_editable = ['description', ]
+    prepopulated_fields = {'slug': ('name', )}
+
+
