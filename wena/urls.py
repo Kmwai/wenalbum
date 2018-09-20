db04c9af73db3d54e8a9a570b74977d6b7f6e37d
Mwai <mwai@Misfit.local>
2018-09-20 14:22:55 +0100

    updates

---
+++ wena/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'wena'
+
+urlpatterns = [
+    path('', views.photo_list, name='photo_list'),
+    path('<str:location_slug>', views.photo_list, name='photo_list_by_location'),
+    path('<int:id>/<str:slug>', views.photo_detail, name='photo_detail'),
+
+]
--- wena/views.py
+++ wena/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Location, Photo
 
-# Create your views here.
+
+def photo_list(request, location_slug=None):
+    location = None
+    locations = Location.objects.all()
+    photos = Photo.objects.filter()
+    if location_slug:
+        location = get_object_or_404(Location, slug=location_slug)
+        photos = locations.filter(location=location)
+    return render(request, '', {'location': location,
+                                'locations': locations,
+                                'photos': photos})
+
+
+def photo_detail(request, id, slug):
+    photo = get_object_or_404(Photo, id=id, slug=slug)
+    return render(request, '', {'photo': photo})
+
+
+
+
