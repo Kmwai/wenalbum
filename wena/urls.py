cb7767bacb2444ac39db0c781fc3e5917fd0e9f1
Mwai <mwai@Misfit.home>
2018-09-27 02:10:46 +0100

    update URLConf

--- wena/urls.py
+++ wena/urls.py
@@ -6,7 +6,7 @@ app_name = 'wena'
 
 urlpatterns = [
     path('', views.photo_list, name='photo_list'),
-    path('<str:location_slug>', views.photo_list, name='photo_list_by_location'),
-    path('<int:id>/<str:slug>', views.photo_detail, name='photo_detail'),
+    path('<str:location_slug>/', views.photo_list, name='photo_list_by_location'),
+    path('<int:id>/<str:slug>/', views.photo_detail, name='photo_detail'),
 
 ]
