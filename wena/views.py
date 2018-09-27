fdb3b6621fd894c9cab84a601c9a10b6215d48e0
Mwai <mwai@Misfit.home>
2018-09-27 02:08:54 +0100

    fix view context

--- wena/views.py
+++ wena/views.py
@@ -3,17 +3,25 @@ from .models import Location, Photo
 
 
 def photo_list(request, location_slug=None):
+    """
+    View function to display all the photos
+
+    """
     location = None
     locations = Location.objects.all()
-    photos = Photo.objects.filter()
+    photos = Photo.objects.all()
     if location_slug:
         location = get_object_or_404(Location, slug=location_slug)
-        photos = locations.filter(location=location)
+        photos = photos.filter(location=location)
     return render(request, 'wena/photos/list.html', {'location': location,
                                                      'locations': locations,
                                                      'photos': photos})
 
 
 def photo_detail(request, id, slug):
+    """
+    View function to display a single instance of a photo
+
+    """
     photo = get_object_or_404(Photo, id=id, slug=slug)
     return render(request, 'wena/photos/detail.html', {'photo': photo})
