0f2ec47ab6909218c42851cde60943d95aefaf67
Mwai <mwai@Misfit.home>
2018-09-27 02:03:51 +0100

    fixed foreign key error in Photo model

--- wena/models.py
+++ wena/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from django.utils import timezone
 from django.urls import reverse
 
 
 class Location(models.Model):
+    """
+    Model representing a location (e.g Lisbon).
+    """
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
@@ -13,14 +15,23 @@ class Location(models.Model):
         verbose_name_plural = 'locations'
 
     def __str__(self):
+        """
+        String for representing the Model object
+        """
         return self.name
 
     def get_absolute_url(self):
+        """
+        returns the url to a particular location instance
+        """
         return reverse('wena:photo_list_by_location', args=[self.slug])
 
 
 class Photo(models.Model):
-    category = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='photos')
+    """
+    Model representing a photo (but not a specific photo)
+    """
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='photos')
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='pictures/%Y/%m/%d/', blank=True)
@@ -34,7 +45,13 @@ class Photo(models.Model):
         index_together = (('id', 'slug'),)  # For querying photos by both id and slug (also to improve performance)
 
     def __str__(self):
+        """
+        String for representing the Model object
+        """
         return self.name
 
     def get_absolute_url(self):
+        """
+        returns the url of a particular photo instance
+        """
         return reverse('wena:photo_detail', args=[self.id, self.slug])
