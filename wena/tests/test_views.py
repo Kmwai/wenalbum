58a750fe5996f830953d83383d3dd9d6dc748477
Mwai Karimi <mwaik@outlook.com>
2018-10-04 17:32:53 +0100

    requirements

---
+++ wena/tests/test_views.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from django.urls import reverse
+from wenalbum.wena.models import Location, Photo
+from wenalbum.wena.views import photo_detail, photo_list
+
+
+class PhotoDetailViewTest(TestCase):
+    pass
+
+
+class PhotoListViewTest(TestCase):
+    pass
+
+
