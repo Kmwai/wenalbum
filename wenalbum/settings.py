b346ae07f4b012bc956f75a0050cfaf9c936eec3
Mwai Karimi <mwaik@outlook.com>
2018-10-05 01:03:45 +0100

    updated ALLOWED_HOSTS

--- wenalbum/settings.py
+++ wenalbum/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '85@-(us8wzphp7%j8sb)av4-xbifai
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wenalbum.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
